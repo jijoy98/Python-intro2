43251d199d030858bf5269e1cbe76dc6c21b3ebf
jijoy98 <90660002+jijoy98@users.noreply.github.com>
2025-01-23 08:53:55 -0500

    Add files via upload

---
+++ project.py
@@ -0,0 +1,12 @@
+name=  input("what is your name?")
+print("hi there, " + name)
+print(name + " is a lovely name.")
+print("hi" + name + "! let me make sure your name is" + name +"?")
+email = input("what is your email?")
+nickname = input("what is your nickname?")
+address = input("what is your address?")
+
+age= int=(input("please enter your age: "))
+
+password = input("what is your password?")
+check = input("what is your checkes?")
