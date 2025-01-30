9e1e8eeb0faa29edfe771aa06f572ea966b1d4a4
jijoy98 <90660002+jijoy98@users.noreply.github.com>
2025-01-30 08:21:08 -0500

    Add files via upload

---
+++ list.py
@@ -0,0 +1,15 @@
+#problem1
+nums =[1,2,3,4,5,6,7,8,9,10]
+nums.append(11)
+print(nums)
+nums.remove(5)
+print(nums)
+nums.reverse()
+print(nums)
+
+#problem22
+nums_2= [12,14,16,18,20]
+print(max(nums_2))
+print(min(nums_2))
+avg = sum(nums) / len(nums_2)
+print(avg)
