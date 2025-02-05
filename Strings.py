29aaa9ca53ff1d99aefdccc757a02df70add43fa
jijoy98 <90660002+jijoy98@users.noreply.github.com>
2025-02-05 11:57:44 -0500

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,24 @@
+#python String
+text = "Hello Jijoy"
+print(text)
+
+#string Concatenation
+str1 = "Hi"
+str2 = "King"
+result = str1 + " " + str2
+print(result)
+
+#String Slices
+text = "Hello Immaculata University"
+print(text[0:8])
+print(text[:10])
+print(text[15:])
+print(text[4:10])
+
+# if Statement
+age = 26
+if age >=20:
+    print( "you are to young for game")
+    if age == '10' and age < 20:
+        print ("you neeed to go back to bed")
+
--- list.py
+++ list.py
@@ -1,15 +1,23 @@
-#problem1
-nums =[1,2,3,4,5,6,7,8,9,10]
-nums.append(11)
-print(nums)
-nums.remove(5)
-print(nums)
-nums.reverse()
-print(nums)
+#List order
+colors = ['black', 'red', 'pink', 'green']
+print(colors[3])
+print(colors[2])
+print(len(colors))
 
-#problem22
-nums_2= [12,14,16,18,20]
-print(max(nums_2))
-print(min(nums_2))
-avg = sum(nums) / len(nums_2)
-print(avg)
+#FOR and IN
+squares = [1,4,7,9]
+sum = 2
+for num in squares:
+    sum+= num
+print(sum)
+
+list = [' Jack', 'love', 'kind']
+if 'love' in list:
+    print ('awsome')
+    
+#LOOP
+d = list(range(30))  # Example list with 30 numbers [0, 1, 2, ..., 29]
+q = 10  # Start from index 10
+while q < len(d):
+    print(d[q])
+    q += 20  # Increment by 20
